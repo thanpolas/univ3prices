510a4f65e64a6aac422877082290cdbd30887eec
Thanos Polychronakis <thanpolas@gmail.com>
2021-07-19 19:44:01 +0300

    lint tester lib

--- test/lib/tester.lib.js
+++ test/lib/tester.lib.js
@@ -1,18 +1,17 @@
 /**
- * @fileOverview Main testing helper lib.
- *
+ * @fileoverview Main testing helper lib.
  */
 
-var tester = module.exports = {};
+const tester = (module.exports = {});
 
 /**
  * Have a Cooldown period between tests.
  *
  * @param {number} seconds cooldown in seconds.
- * @return {Function} use is beforeEach().
+ * @return {function} use is beforeEach().
  */
-tester.cooldown = function(seconds) {
-  return function(done) {
+tester.cooldown = function (seconds) {
+  return function (done) {
     setTimeout(done, seconds);
   };
 };
