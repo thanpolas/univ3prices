f5ae54eafb26a196288c185db3e6b6ac8a11a0df
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-04 20:56:39 +0300

    create univ3 constants

---
+++ src/constants.js
@@ -0,0 +1,25 @@
+/**
+ * @fileoverview Required Uniswap V3 constants.
+ */
+
+const JSBI = require('jsbi');
+
+const consts = {};
+
+/** @const {number} RESOLUTION fixed point resolution  */
+consts.RESOLUTION = 96;
+
+// constants used internally but not expected to be used externally
+consts.NEGATIVE_ONE = JSBI.BigInt(-1);
+consts.ZERO = JSBI.BigInt(0);
+consts.ONE = JSBI.BigInt(1);
+consts.TWO = JSBI.BigInt(2);
+
+// used in liquidity amount math
+consts.Q32 = JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(32));
+consts.Q96 = JSBI.exponentiate(JSBI.BigInt(2), JSBI.BigInt(96));
+consts.Q192 = JSBI.exponentiate(consts.Q96, JSBI.BigInt(2));
+
+consts.MaxUint256 = JSBI.BigInt(
+  '0xffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffffff',
+);
