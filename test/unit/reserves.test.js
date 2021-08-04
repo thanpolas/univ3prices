8f12c72fda021df04a1d6bdd8838970b1b92b54d
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-04 23:08:13 +0300

    cleanup reserves tests

--- test/unit/reserves.test.js
+++ test/unit/reserves.test.js
@@ -2,30 +2,100 @@
  * @fileoverview Test reserves calculation.
  */
 
-// const JSBI = require('jsbi');
 const univ3Price = require('../..');
 // const subfix = require('../fixtures/subgraph-results.fix');
+const { encodeSqrtRatioX96: encodePriceSqrt } = univ3Price;
 
-const { reserves } = univ3Price;
+const { reserves, getAmountsForLiquidity } = univ3Price;
 
 describe('Uniswap V3 Reserves', () => {
-  describe('toSignificant', () => {
-    describe('DAI/USDC', () => {
-      it('Reserves of token0 and token1 should be as expected', () => {
-        // Using DAI/WETH: https://etherscan.io/address/0x60594a405d53811d3bc4766596efd80fd545a270
+  it('Reserves of token0 and token1 should be as expected', () => {
+    // Using DAI/WETH: https://etherscan.io/address/0x60594a405d53811d3bc4766596efd80fd545a270
 
-        const liquidity = '2830981547246997099758055';
-        const sqrtPrice = '1550724133884968571999296281';
-        const tickSpacing = '60';
-        const [token0Reserves, token1Reserves] = reserves(
-          liquidity,
-          sqrtPrice,
-          tickSpacing,
-        );
-        console.log(
-          `token0 reserves: "${token0Reserves}"\ntoken1 reserves: "${token1Reserves}"`,
-        );
-      });
+    const liquidity = '2830981547246997099758055';
+    const sqrtPrice = '1550724133884968571999296281';
+    const tickSpacing = '60';
+    const [token0Reserves, token1Reserves] = reserves(
+      liquidity,
+      sqrtPrice,
+      tickSpacing,
+    );
+    console.log(
+      `token0 reserves: "${token0Reserves}"\ntoken1 reserves: "${token1Reserves}"`,
+    );
+  });
+
+  describe('uniswap-v3-periphery spec', () => {
+    it('amounts for price inside', () => {
+      const sqrtPriceX96 = encodePriceSqrt(1, 1);
+      const sqrtPriceAX96 = encodePriceSqrt(100, 110);
+      const sqrtPriceBX96 = encodePriceSqrt(110, 100);
+      const [amount0, amount1] = getAmountsForLiquidity(
+        sqrtPriceX96,
+        sqrtPriceAX96,
+        sqrtPriceBX96,
+        2148,
+      );
+
+      expect(Number(amount0)).toEqual(99);
+      expect(Number(amount1)).toEqual(99);
+    });
+
+    it('amounts for price below', () => {
+      const sqrtPriceX96 = encodePriceSqrt(99, 110);
+      const sqrtPriceAX96 = encodePriceSqrt(100, 110);
+      const sqrtPriceBX96 = encodePriceSqrt(110, 100);
+      const [amount0, amount1] = getAmountsForLiquidity(
+        sqrtPriceX96,
+        sqrtPriceAX96,
+        sqrtPriceBX96,
+        1048,
+      );
+
+      expect(Number(amount0)).toEqual(99);
+      expect(Number(amount1)).toEqual(0);
+    });
+
+    it('amounts for price above', () => {
+      const sqrtPriceX96 = encodePriceSqrt(111, 100);
+      const sqrtPriceAX96 = encodePriceSqrt(100, 110);
+      const sqrtPriceBX96 = encodePriceSqrt(110, 100);
+      const [amount0, amount1] = getAmountsForLiquidity(
+        sqrtPriceX96,
+        sqrtPriceAX96,
+        sqrtPriceBX96,
+        2097,
+      );
+      expect(Number(amount0)).toEqual(0);
+      expect(Number(amount1)).toEqual(199);
+    });
+
+    it('amounts for price on lower boundary', () => {
+      const sqrtPriceAX96 = encodePriceSqrt(100, 110);
+      const sqrtPriceX96 = sqrtPriceAX96;
+      const sqrtPriceBX96 = encodePriceSqrt(110, 100);
+      const [amount0, amount1] = getAmountsForLiquidity(
+        sqrtPriceX96,
+        sqrtPriceAX96,
+        sqrtPriceBX96,
+        1048,
+      );
+      expect(Number(amount0)).toEqual(99);
+      expect(Number(amount1)).toEqual(0);
+    });
+
+    it('amounts for price on upper boundary', () => {
+      const sqrtPriceAX96 = encodePriceSqrt(100, 110);
+      const sqrtPriceBX96 = encodePriceSqrt(110, 100);
+      const sqrtPriceX96 = sqrtPriceBX96;
+      const [amount0, amount1] = getAmountsForLiquidity(
+        sqrtPriceX96,
+        sqrtPriceAX96,
+        sqrtPriceBX96,
+        2097,
+      );
+      expect(Number(amount0)).toEqual(0);
+      expect(Number(amount1)).toEqual(199);
     });
   });
 });
