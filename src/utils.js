a4bd5ba044c744603c39b9102630a9ba21e071c1
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-05 11:52:09 +0300

    create expDecs() and biConv() utility fns

--- src/utils.js
+++ src/utils.js
@@ -65,3 +65,27 @@ utils.tickRange = (tickStr, tickSpacingStr) => {
   const tickHigh = tickLow + tickSpacing;
   return [tickLow, tickHigh];
 };
+
+/**
+ * Will calculate the exponent for the given decimals number.
+ *
+ * @param {string|number} decs The decimal number.
+ * @return {JSBI} The decimals exponent.
+ */
+utils.expDecs = (decs) => {
+  return JSBI.exponentiate(JSBI.BigInt(10), JSBI.BigInt(decs));
+};
+
+/**
+ * Converts a value to JSBI, if it's already a JSBI will just return it.
+ *
+ * @param {string|number|JSBI} numstr The value to convert.
+ * @return {JSBI} JSBI representation of the value.
+ */
+utils.biConv = (numstr) => {
+  let bi = numstr;
+  if (typeof sqrtRatio !== 'bigint') {
+    bi = JSBI.BigInt(numstr);
+  }
+  return bi;
+};
