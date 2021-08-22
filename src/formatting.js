b0f015f80a447fdb0d418ccc4b7ff807a4a52e4a
Thanos Polychronakis <thanpolas@gmail.com>
2021-08-22 13:31:22 +0300

    decouple fraction calculation and formatting to crypto-utils

--- src/formatting.js
+++ src/formatting.js
@@ -1,88 +1,12 @@
 /**
  * @fileoverview Converts Big Integers to human readable format.
+ * @see https://github.com/thanpolas/crypto-utils
  */
 
-const Big = require('big.js');
-const invariant = require('invariant');
-const toFormat = require('toformat');
-const Decimal = require('decimal.js');
-
-const { toSignificantRounding, toFixedRounding } = require('./rounding');
-const { Rounding } = require('./constants');
-
-const DecimalFormat = toFormat(Decimal);
-const BigFormat = toFormat(Big);
+const { toSignificant, toFixed, toAuto } = require('@thanpolas/crypto-utils');
 
 const formatting = (module.exports = {});
 
-/**
- * Will convert the fraction of the two tokens into a significant representation.
- *
- * @param {Array<bigint>} fraction Fraction tupple array containing the numerator
- *    and denominator.
- * @param {number=} significantDigits How many significant digits to use.
- * @param {Object=} format Formatting of the output.
- * @param {Rounding} rounding Desired rounding.
- * @return {string} The result.
- */
-formatting.toSignificant = (
-  fraction,
-  significantDigits = 5,
-  format = { groupSeparator: '' },
-  rounding = Rounding.ROUND_HALF_UP,
-) => {
-  invariant(
-    Number.isInteger(significantDigits),
-    `${significantDigits} is not an integer.`,
-  );
-  invariant(significantDigits > 0, `${significantDigits} is not positive.`);
-
-  const precision = significantDigits + 1;
-  const roundingDecimal = toSignificantRounding[rounding];
-
-  Decimal.set({ precision, rounding: roundingDecimal });
-
-  const [numerator, denominator] = fraction;
-
-  const quotient = new DecimalFormat(numerator.toString())
-    .div(denominator.toString())
-    .toSignificantDigits(significantDigits);
-
-  const res = quotient.toFormat(quotient.decimalPlaces(), format);
-
-  return res;
-};
-
-/**
- * Will convert the fraction of the two tokens into a fixed representation.
- *
- * @param {Array<bigint>} fraction Fraction tupple Array containing the numerator
- *    and denominator.
- * @param {number=} decimalPlaces How many decimal places to use.
- * @param {Object=} format Formatting of the output.
- * @param {Rounding} rounding Desired rounding.
- * @return {string} The result.
- */
-formatting.toFixed = (
-  fraction,
-  decimalPlaces = 5,
-  format = { groupSeparator: '' },
-  rounding = Rounding.ROUND_HALF_UP,
-) => {
-  invariant(
-    Number.isInteger(decimalPlaces),
-    `${decimalPlaces} is not an integer.`,
-  );
-  invariant(decimalPlaces >= 0, `${decimalPlaces} is negative.`);
-
-  BigFormat.DP = decimalPlaces;
-  BigFormat.RM = toFixedRounding[rounding];
-
-  const [numerator, denominator] = fraction;
-
-  const res = new BigFormat(numerator.toString())
-    .div(denominator.toString())
-    .toFormat(decimalPlaces, format);
-
-  return res;
-};
+formatting.toSignificant = toSignificant;
+formatting.toFixed = toFixed;
+formatting.toAuto = toAuto;
